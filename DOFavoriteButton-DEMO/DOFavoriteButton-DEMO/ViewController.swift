8ba524443e7b548207c847007022f167b052c214
Daiki Okumura <daiki.okumura@gmail.com>
2015-07-09 02:13:15 +0900

    initial commit

---
+++ DOFavoriteButton-DEMO/DOFavoriteButton-DEMO/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  DOFavoriteButton-DEMO
+//
+//  Created by Daiki Okumura on 2015/07/09.
+//  Copyright (c) 2015 Daiki Okumura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = (self.view.frame.width - 44) / 4
+        var x = width / 2
+        let y = self.view.frame.height / 2 - 22
+        
+        // star button
+        let starButton = DOFavoriteButton(frame: CGRectMake(x, y, 44, 44), image: UIImage(named: "star"), imageFrame: CGRectMake(12, 12, 20, 20))
+        starButton.addTarget(self, action: Selector("tappedButton:"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(starButton)
+        x += width
+        
+        // heart button
+        let heartButton = DOFavoriteButton(frame: CGRectMake(x, y, 44, 44), image: UIImage(named: "heart"), imageFrame: CGRectMake(12, 12, 20, 20))
+        heartButton.imageColorOn = UIColor(red: 254/255, green: 110/255, blue: 111/255, alpha: 1.0)
+        heartButton.circleColor = UIColor(red: 254/255, green: 110/255, blue: 111/255, alpha: 1.0)
+        heartButton.lineColor = UIColor(red: 226/255, green: 96/255, blue: 96/255, alpha: 1.0)
+        heartButton.addTarget(self, action: Selector("tappedButton:"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(heartButton)
+        x += width
+        
+        // like button
+        let likeButton = DOFavoriteButton(frame: CGRectMake(x, y, 44, 44), image: UIImage(named: "like"), imageFrame: CGRectMake(12, 12, 20, 20))
+        likeButton.imageColorOn = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
+        likeButton.circleColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
+        likeButton.lineColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0)
+        likeButton.addTarget(self, action: Selector("tappedButton:"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(likeButton)
+        x += width
+        
+        // smile button
+        let smileButton = DOFavoriteButton(frame: CGRectMake(x, y, 44, 44), image: UIImage(named: "smile"), imageFrame: CGRectMake(12, 12, 20, 20))
+        smileButton.imageColorOn = UIColor(red: 45/255, green: 204/255, blue: 112/255, alpha: 1.0)
+        smileButton.circleColor = UIColor(red: 45/255, green: 204/255, blue: 112/255, alpha: 1.0)
+        smileButton.lineColor = UIColor(red: 45/255, green: 195/255, blue: 106/255, alpha: 1.0)
+        smileButton.addTarget(self, action: Selector("tappedButton:"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(smileButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tappedButton(sender: DOFavoriteButton) {
+        if sender.selected {
+            sender.deselect()
+        } else {
+            sender.select()
+        }
+    }
+}
+
