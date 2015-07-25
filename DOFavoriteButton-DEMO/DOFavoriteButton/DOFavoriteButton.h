eaf4551ff11be1b53e1c583189bc72dfdd30eabf
Daiki Okumura <daiki.okumura@gmail.com>
2015-07-25 16:12:09 +0900

    support Carthage (fixed issue #3)

---
+++ DOFavoriteButton-DEMO/DOFavoriteButton/DOFavoriteButton.h
@@ -0,0 +1,19 @@
+//
+//  DOFavoriteButton.h
+//  DOFavoriteButton
+//
+//  Created by Daiki Okumura on 2015/07/25.
+//  Copyright (c) 2015 Daiki Okumura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DOFavoriteButton.
+FOUNDATION_EXPORT double DOFavoriteButtonVersionNumber;
+
+//! Project version string for DOFavoriteButton.
+FOUNDATION_EXPORT const unsigned char DOFavoriteButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DOFavoriteButton/PublicHeader.h>
+
+
